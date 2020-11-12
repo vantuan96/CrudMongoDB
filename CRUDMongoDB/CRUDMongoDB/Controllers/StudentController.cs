3694d3c990d8a10010b1aa615830123a73d1d1f1
Vantuan96 <vantuan1996197@gmail.com>
2020-11-13 05:55:49 +0700

    no message

--- CRUDMongoDB/CRUDMongoDB/Controllers/StudentController.cs
+++ CRUDMongoDB/CRUDMongoDB/Controllers/StudentController.cs
@@ -20,43 +20,43 @@ namespace CRUDMongoDB.Controllers
             //var gridmodel = await StudentService.GetList();
             return View();
         }
-      //  [HttpGet]
-        //public async Task<IActionResult> Create(Student model)
-        //{
-        //    model = model == null ? new Student() : model;
-        //    return View(await Task.FromResult(model));
-        //}
-        //[HttpPost]
-        //public async Task<IActionResult> Create(Student model, bool SaveAndCountinue = false)
-        //{
-        //    //if (!ModelState.IsValid)
-        //    //{
-        //    //    return View(model);
-        //    //}
-        //    //var obj = new Student()
-        //    //{
-        //    //    Id = model.Id,
-        //    //    FullName = model.FullName,
-        //    //    Age = model.Age,
-        //    //    DateCreate = model.DateCreate,
-        //    //    IsDelete = model.IsDelete
+        [HttpGet]
+        public async Task<IActionResult> Create(Student model)
+        {
+            model = model == null ? new Student() : model;
+            return View(await Task.FromResult(model));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Student model, bool SaveAndCountinue = false)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var obj = new Student()
+            {
+                Id = model.Id,
+                FullName = model.FullName,
+                Age = model.Age,
+                DateCreate = model.DateCreate,
+                IsDelete = model.IsDelete
 
-        //    //};
-        //    //// thuc hientheem mới
-        //    //var result = await StudentService.Create(obj);
-        //    //if (result.isSuccess)
-        //    //{
-        //    //    if (SaveAndCountinue)
-        //    //    {
-        //    //        TempData["Success"] = "Thêm mới thành công";
-        //    //        return RedirectToAction("Create");
-        //    //    }
+            };
+            // thuc hientheem mới
+            var result = await StudentService.Create(obj);
+            if (result.isSuccess)
+            {
+                if (SaveAndCountinue)
+                {
+                    TempData["Success"] = "Thêm mới thành công";
+                    return RedirectToAction("Create");
+                }
 
-        //    //    return RedirectToAction("Index");
+                return RedirectToAction("Index");
 
-        //    //}
-        //    //return RedirectToAction("Index");
-        //    return null;
-        //}
+            }
+            return RedirectToAction("Index");
+            
+        }
     }
 }
--- CRUDMongoDB/CRUDMongoDB/Program.cs
+++ CRUDMongoDB/CRUDMongoDB/Program.cs
@@ -2,25 +2,45 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace CRUDMongoDB
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    //    public static void Main(string[] args)
+        //    {
+        //        CreateHostBuilder(args).Build().Run();
+        //    }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        //    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //  Host.CreateDefaultBuilder(args)
+        //    .ConfigureWebHostDefaults(builder =>
+        //    {
+        //        builder.ConfigureKestrel(opts =>
+        //        {
+        //          // ...
+        //      })
+        //        .UseStartup<Startup>();
+        //    })
+        //    .ConfigureServices(services =>
+        //    {
+        //        //services.AddHostedService<"">();
+        //    });
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                CreateWebHostBuilder(args).Build().Run();
+            }
+
+            public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+              WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+        }
     }
-}
+
+
